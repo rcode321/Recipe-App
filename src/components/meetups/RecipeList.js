42756c539d5092fe76648337155a4e946a25fb31
Raf <rafaelmendoza004@gmail.com>
2022-04-07 21:29:34 +0800

    refactor

--- src/components/meetups/RecipeList.js
+++ src/components/meetups/RecipeList.js
@@ -1,10 +1,18 @@
 import classes from "./RecipeStyles.module.css";
-
 import * as React from "react";
 import { Box } from "@mui/system";
-import { Button, CardActions, CardContent, Typography } from "@mui/material";
-import Card from "@mui/material/Card";
-
+import {
+	Avatar,
+	List,
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+	Typography,
+} from "@mui/material";
+import OutdoorGrillIcon from "@mui/icons-material/OutdoorGrill";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import DateRangeIcon from "@mui/icons-material/DateRange";
 const RecipeList = ({
 	directions,
 	ingredients,
@@ -18,45 +26,52 @@ const RecipeList = ({
 	images,
 	data,
 }) => {
-	const bull = (
-		<Box
-			component="span"
-			sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-		>
-			•
-		</Box>
-	);
 	return (
-		<Box>
-			<Typography variant="h3"> {title}</Typography>
+		<Box className={classes.container}>
+			<Box m={4}>
+				<Typography variant="h3"> {title}</Typography>
+				<Typography variant="subtitle1"> {description}</Typography>
+			</Box>
 			<Box className={classes.image}>
 				<img src={images} alt={title} />
 			</Box>
-
-			<Card sx={{ maxWidth: 275 }} className={classes.card}>
-				<CardContent>
-					<Typography variant="h4" color="text.primary" gutterBottom>
-						{description}
-					</Typography>
-					<Typography variant="body1" component="div">
-						{`Cooktime - ${cookTime} min `}
-					</Typography>
-					<Typography variant="body1" component="div">
-						{`Servings - ${servings} Person`}
-					</Typography>
-					<Typography variant="body1" component="div">
-						{`Preptime - ${prepTime} min`}
-					</Typography>
-					<br />
-					<Typography variant="subtitle2" sx={{ mb: 1.5 }} color="text.secondary">
-						{editDate}
-					</Typography>
-
-					<Typography variant="subtitle2" sx={{ mb: 1.5 }} color="text.secondary">
-						{postDate}
-					</Typography>
-				</CardContent>
-			</Card>
+			<Box className={classes.cards}>
+				<List sx={{ width: "100%", maxWidth: 360 }}>
+					<ListItem>
+						<ListItemAvatar>
+							<Avatar>
+								<OutdoorGrillIcon color="primary" />
+							</Avatar>
+						</ListItemAvatar>
+						<ListItemText primary="Cooktime" secondary={`${cookTime} minutes`} />
+					</ListItem>
+					<ListItem>
+						<ListItemAvatar>
+							<Avatar>
+								<RestaurantMenuIcon color="primary" />
+							</Avatar>
+						</ListItemAvatar>
+						<ListItemText primary="Servings" secondary={`${servings} persons`} />
+					</ListItem>
+					<ListItem>
+						<ListItemAvatar>
+							<Avatar>
+								<AccessTimeIcon color="primary" />
+							</Avatar>
+						</ListItemAvatar>
+						<ListItemText primary="Preptime" secondary={`${prepTime} minutes`} />
+					</ListItem>
+					<ListItem>
+						<ListItemAvatar>
+							<Avatar>
+								<DateRangeIcon color="primary" />
+							</Avatar>
+						</ListItemAvatar>
+						<ListItemText secondary={`${editDate} `} />
+						<ListItemText secondary={`${postDate} `} />
+					</ListItem>
+				</List>
+			</Box>
 		</Box>
 	);
 };
